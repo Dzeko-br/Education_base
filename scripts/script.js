9621891918ff4a459fd3545c629e1dce42285a96
Dmitrich <andrey.arsenev@gmail.com>
2021-05-04 00:52:53 +0300

    ТЗ 3 week

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //menu
+    const gamburgerBtn = document.querySelector('.humburger');
+    const mobileMenu = document.querySelector('.mobile-menu');
+    const closeBtn = mobileMenu.querySelector('.mobile-menu__close');
+
+    const showMenu = e => {
+        e.preventDefault();
+        mobileMenu.classList.toggle('mobile-menu--active');
+    }
+
+    gamburgerBtn.addEventListener('click', (e) => showMenu(e));
+    closeBtn.addEventListener('click', (e) => showMenu(e));
+
+});
